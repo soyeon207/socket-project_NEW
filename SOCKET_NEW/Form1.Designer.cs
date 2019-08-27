c156feeeb1b592144dfb402dad1d5fa4f60591d0
soyeon207 <psy010207@naver.com>
2019-08-28 01:33:42 +0900

    [Add] 첫번째화면 front design & button click event

---
+++ SOCKET_NEW/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SOCKET_NEW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.exit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.server = new System.Windows.Forms.Button();
+            this.client = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(139, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NEW";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.server);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(23, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 143);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "서버";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.client);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(23, 201);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(290, 143);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "클라이언트";
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(23, 366);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(290, 40);
+            this.exit.TabIndex = 0;
+            this.exit.Text = "종료";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "채팅창을 엽니다";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "채팅창에 참여합니다";
+            // 
+            // server
+            // 
+            this.server.Location = new System.Drawing.Point(68, 71);
+            this.server.Name = "server";
+            this.server.Size = new System.Drawing.Size(152, 35);
+            this.server.TabIndex = 2;
+            this.server.Text = "SERVER 시작";
+            this.server.UseVisualStyleBackColor = true;
+            this.server.Click += new System.EventHandler(this.Server_Click);
+            // 
+            // client
+            // 
+            this.client.Location = new System.Drawing.Point(68, 71);
+            this.client.Name = "client";
+            this.client.Size = new System.Drawing.Size(152, 35);
+            this.client.TabIndex = 3;
+            this.client.Text = "Client 시작";
+            this.client.UseVisualStyleBackColor = true;
+            this.client.Click += new System.EventHandler(this.Client_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 440);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button server;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button client;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ SOCKET_NEW/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOCKET_NEW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void Client_Click(object sender, EventArgs e)
+        {
+            Client client = new Client();
+            client.Show();
+        }
+
+        private void Server_Click(object sender, EventArgs e)
+        {
+            Server server = new Server();
+            server.Show();
+        }
+    }
+}
